c2f924825daf35406f30c758116986a5ac27bf78
juliocezarpaiva <35053862+juliocezarpaiva@users.noreply.github.com>
2021-07-29 11:07:30 -0300

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+# djang-mail configs:
+ EMAIL_HOST = o servidor smtp do seu email (Google: 'smtp.gmail.com')
+ EMAIL_USE_TLS = verifique se o seu provedor usa TLS (Google: True)
+ EMAIL_PORT = verifique a porta usada pelo seu provedor (Google: 587)
+ EMAIL_HOST_USER = 'email@provedor.com'
+ EMAIL_HOST_PASSWORD = 'senha'
+
+ # api configs:
+ API_KEY = { 'sua api key' }
